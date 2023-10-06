3904abdedd997ae1b47cd6613cdae2405680f96a
frankpagan <frank@cocreate.app>
2023-10-05 19:53:53 -0600

    feat: Persists config data in the client - starts with localStorage, fallback to cookie or map

--- src/client.js
+++ src/client.js
@@ -1,10 +1,15 @@
+import localStorage from '@cocreate/local-storage'
+
 // Function to get a value by key from localStorage or cookies
 function get(key) {
-    if (isLocalStorageSupported()) {
-        // If localStorage is supported, try to get the value from it
-        return localStorage.getItem(key);
+    let value
+    value = localStorage.getItem(key)
+
+    if (window.CoCreateConfig && window.CoCreateConfig[key])
+        return window.CoCreateConfig[key]
+    else if (value = localStorage.getItem(key)) {
+        return value
     } else {
-        // If localStorage is not supported, try to get the value from cookies
         const cookies = document.cookie.split("; ");
         for (const cookie of cookies) {
             const [cookieName, cookieValue] = cookie.split("=");
@@ -12,46 +17,25 @@ function get(key) {
                 return decodeURIComponent(cookieValue);
             }
         }
-        return null; // Key not found in cookies
     }
 }
 
 // Function to set a key-value pair in localStorage or cookies
 function set(key, value) {
-    if (isLocalStorageSupported()) {
-        // If localStorage is supported, set the value in it
-        localStorage.setItem(key, value);
-    } else {
-        // If localStorage is not supported, set the value in cookies
-        document.cookie = `${key}=${encodeURIComponent(value)}`;
-    }
-}
-
-// Function to check if a key exists in localStorage or cookies
-function has(key) {
-    if (isLocalStorageSupported()) {
-        // If localStorage is supported, check if the key exists
-        return localStorage.getItem(key) !== null;
-    } else {
-        // If localStorage is not supported, check if the key exists in cookies
-        const cookies = document.cookie.split("; ");
-        for (const cookie of cookies) {
-            const [cookieName] = cookie.split("=");
-            if (cookieName === key) {
-                return true;
-            }
-        }
-        return false; // Key not found in cookies
-    }
+    if (!window.CoCreateConfig)
+        window.CoCreateConfig = { [key]: value }
+    else
+        window.CoCreateConfig[key] = value
+    localStorage.setItem(key, value)
+    document.cookie = `${key}=${encodeURIComponent(value)}`;
 }
 
 // Function to delete a key-value pair from localStorage or cookies
-function del(key) {
-    if (isLocalStorageSupported()) {
-        // If localStorage is supported, remove the key-value pair
-        localStorage.removeItem(key);
-    } else {
-        // If localStorage is not supported, expire the cookie to delete it
-        document.cookie = `${key}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
-    }
+function remove(key) {
+    if (!window.CoCreateConfig)
+        delete window.CoCreateConfig[key]
+    localStorage.removeItem(key)
+    document.cookie = `${key}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
 }
+
+export default { get, set, remove }
