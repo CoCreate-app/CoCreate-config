d6e0b2266674c34582cdddc0f36f6836ba1c11f1
frankpagan <frank@cocreate.app>
2023-11-25 15:33:57 -0600

    feat: get config using CoCreate-config

--- src/server.js
+++ src/server.js
@@ -4,6 +4,42 @@ const path = require('path');
 const fs = require('fs');
 const { dotNotationToObject, getValueFromObject } = require('@cocreate/utils');
 
+/**
+ * Asynchronously retrieves configuration values. This function allows for 
+ * fetching values from project-specific configuration, global configuration, or 
+ * environment variables. Project configuration takes precedence over global and 
+ * environment settings. It also supports interactive prompts to gather critical values 
+ * from the user if they are not already defined in the configuration.
+ *
+ * @async
+ * @param {Object} items - An object representing the configuration items to retrieve. 
+ *                         The keys are the config paths, and the values are objects 
+ *                         specifying the prompt and any variable substitutions.
+ * @param {boolean} [env=true] - Flag to consider environment variables during config lookup.
+ * @param {boolean} [global=true] - Flag to consider global configuration during config lookup.
+ * @param {string} [configPath='CoCreate.config.js'] - Path to the project-specific configuration file.
+ * @returns {Promise<Object>} A promise that resolves with the configuration object.
+ *
+ * @example
+ * // Usage of the Config function
+ * async function initConfig() {
+ *   this.config = await Config({
+ *       'organization_id': { prompt: 'Enter your organization_id: ' },
+ *       'storage': {
+ *           prompt: 'Enter a friendly name for the new storage: ',
+ *           variable: 'name'
+ *       },
+ *       'storage.{{name}}.provider': {
+ *           prompt: 'Enter the storage provider, ex mongodb: '
+ *       },
+ *       'storage.{{name}}.url': {
+ *           prompt: 'Enter the storage providers url: '
+ *       }
+ *   });
+ *
+ *   const lazyLoadConfig = await Config('lazyload', false, false);
+ * }
+ */
 module.exports = async function (items, env = true, global = true, configPath = 'CoCreate.config.js') {
     async function promptForInput(question) {
         const rl = readline.createInterface({
@@ -38,6 +74,8 @@ module.exports = async function (items, env = true, global = true, configPath = 
     let variables = {};
 
     async function getConfig(items) {
+        if (typeof items === 'string')
+            items = { [items]: '' }
         for (let key of Object.keys(items)) {
             let { value, prompt, choices, variable } = items[key];
 
